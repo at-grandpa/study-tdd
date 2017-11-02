6da5b0abb5cdb26abe7d4da3c81a41390d5796c5
yoshiyuki-tsuchida <yoshiyuki_tsuchida@voyagegroup.com>
2017-11-02 22:24:22 +0900

    amountの重複排除を始めた

--- chapter01/src/money/dollar.cr
+++ chapter01/src/money/dollar.cr
@@ -1,11 +1,12 @@
 module Money
   class Dollar
-    property amount : Int32 = 10
+    property amount : Int32
 
-    def initialize(amount : Int32)
+    def initialize(@amount : Int32)
     end
 
     def times(multiplier : Int32)
+      @amount = 5 * 2
     end
   end
 end
