98da0fe661a933cdb1d1cb9dcc42054c68f65349
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2017-11-03 00:05:06 +0900

    $5*3=$15になってほしいが、副作用があるためテストが落ちる

--- part01/spec/money_spec.cr
+++ part01/spec/money_spec.cr
@@ -2,10 +2,12 @@ require "./spec_helper"
 
 describe Money do
   describe "multiplication" do
-    it "$5 * 2 = 10 になること" do
+    it "掛け算を計算できること" do
       five = Money::Dollar.new(5)
       five.times(2)
       five.amount.should eq 10
+      five.times(3)
+      five.amount.should eq 15
     end
   end
 end
