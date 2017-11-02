5441c16d8c412bbfd0b937f9880a766ac1d8ea63
yoshiyuki-tsuchida <yoshiyuki_tsuchida@voyagegroup.com>
2017-11-02 18:31:12 +0900

    Dollarクラスがみつかるようになった

--- chapter01/spec/money_spec.cr
+++ chapter01/spec/money_spec.cr
@@ -3,7 +3,7 @@ require "./spec_helper"
 describe Money do
   describe "multiplication" do
     it "$5 * 2 = 10 になること" do
-      five = Dollar.new(5)
+      five = Money::Dollar.new(5)
       five.times(2)
       five.amount.should eq 10
     end
