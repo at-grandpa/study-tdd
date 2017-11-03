3b5238ea1486ce37576333525443797e350d055c
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2017-11-03 23:38:54 +0900

    テストのインターフェースを修正

--- part01/spec/money_spec.cr
+++ part01/spec/money_spec.cr
@@ -4,10 +4,10 @@ describe Money do
   describe "multiplication" do
     it "掛け算を計算できること" do
       five = Money::Dollar.new(5)
-      five.times(2)
-      five.amount.should eq 10
-      five.times(3)
-      five.amount.should eq 15
+      product = five.times(2)
+      product.amount.should eq 10
+      product = five.times(3)
+      product.amount.should eq 15
     end
   end
 end
