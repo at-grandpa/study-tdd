69080c4da4542678a7e01d67bcb25ea7b160da29
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2017-11-06 07:30:44 +0900

    Dollarをfactoryメソッド経由で作成するようにした

--- part01/spec/money_spec.cr
+++ part01/spec/money_spec.cr
@@ -4,17 +4,17 @@ describe MoneyPackage do
   describe "testMultiplication()" do
     it "掛け算を計算できること" do
       five = MoneyPackage::Money.dollar(5)
-      five.times(2).should eq MoneyPackage::Dollar.new(10)
-      five.times(3).should eq MoneyPackage::Dollar.new(15)
+      five.times(2).should eq MoneyPackage::Money.dollar(10)
+      five.times(3).should eq MoneyPackage::Money.dollar(15)
     end
   end
   describe "testEquality()" do
     it "正しく等価性比較ができること" do
-      (MoneyPackage::Dollar.new(5) == MoneyPackage::Dollar.new(5)).should be_true
-      (MoneyPackage::Dollar.new(5) == MoneyPackage::Dollar.new(6)).should be_false
+      (MoneyPackage::Money.dollar(5) == MoneyPackage::Money.dollar(5)).should be_true
+      (MoneyPackage::Money.dollar(5) == MoneyPackage::Money.dollar(6)).should be_false
       (MoneyPackage::Franc.new(5) == MoneyPackage::Franc.new(5)).should be_true
       (MoneyPackage::Franc.new(5) == MoneyPackage::Franc.new(6)).should be_false
-      (MoneyPackage::Franc.new(5) == MoneyPackage::Dollar.new(5)).should be_false
+      (MoneyPackage::Franc.new(5) == MoneyPackage::Money.dollar(5)).should be_false
     end
   end
   describe "testFrancMultiplication()" do
