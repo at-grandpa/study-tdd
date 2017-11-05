869a37a3a30d4f51c47bd5b3541dc35a37c8ed5a
yoshiyuki-tsuchida <yoshiyuki_tsuchida@voyagegroup.com>
2017-11-05 15:43:25 +0900

    Francを追加してグリーンにした

---
+++ part01/src/money/franc.cr
@@ -0,0 +1,12 @@
+module Money
+  class Franc
+    def initialize(@amount : Int32)
+    end
+
+    def times(multiplier : Int32)
+      Franc.new(@amount * multiplier)
+    end
+
+    def_equals @amount
+  end
+end
