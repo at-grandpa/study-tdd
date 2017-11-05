dbbd7500168334be4c6ef73b10f5fca529975e52
yoshiyuki-tsuchida <yoshiyuki_tsuchida@voyagegroup.com>
2017-11-05 14:42:34 +0900

    dollarのテストをコピーしてfrancのテストを追加

--- part01/spec/money_spec.cr
+++ part01/spec/money_spec.cr
@@ -14,4 +14,11 @@ describe Money do
       (Money::Dollar.new(5) == Money::Dollar.new(6)).should be_false
     end
   end
+  describe "testFrancMultiplication()" do
+    it "掛け算を計算できること" do
+      five = Money::Franc.new(5)
+      five.times(2).should eq Money::Franc.new(10)
+      five.times(3).should eq Money::Franc.new(15)
+    end
+  end
 end
