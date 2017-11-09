3edd4a2a13f36e0c596b7e97aa5bb1334a033148
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2017-11-10 07:26:49 +0900

    amountの呼び出し変更

--- part01/src/money_package/bank.cr
+++ part01/src/money_package/bank.cr
@@ -2,7 +2,7 @@ module MoneyPackage
   class Bank
     def reduce(source : Expression, to : String)
       sum = source.as(Sum)
-      amount = sum.augend.amount + sum.addend.amount
+      amount = sum.augend.@amount + sum.addend.@amount
       Money.new(amount, to)
     end
   end
