b19f3be36f71b22c9959463a4db92150af3006a4
yoshiyuki-tsuchida <yoshiyuki_tsuchida@voyagegroup.com>
2017-11-09 09:47:53 +0900

    コメントの削除

--- part01/src/money_package/money.cr
+++ part01/src/money_package/money.cr
@@ -19,7 +19,6 @@ module MoneyPackage
       Money.new(@amount * multiplier, @currency)
     end
 
-    # ch13
     def plus(addend : self) : Expression
       Money.new(@amount + addend.@amount, @currency)
     end
