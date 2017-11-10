1649565bab95fbd3781f4315e95715852b0d9780
yoshiyuki-tsuchida <yoshiyuki_tsuchida@voyagegroup.com>
2017-11-11 06:49:12 +0900

    rateを格納するhashを定義し、addRateとrateのメソッドを書き換えた

--- part01/src/money_package/bank.cr
+++ part01/src/money_package/bank.cr
@@ -1,14 +1,20 @@
 module MoneyPackage
   class Bank
+
+    def initialize
+      @rates = {} of Pair => Int32
+    end
+
     def reduce(source : Expression, to : String) : Money
       source.reduce(self, to)
     end
 
     def addRate(from : String, to : String, rate : Int32) : Void
+      @rates[Pair.new(from, to)] = rate
     end
 
     def rate(from : String, to : String) : Int32
-      (from == "CHF" && to == "USD") ? 2 : 1
+      @rates[Pair.new(from, to)]
     end
   end
 end
