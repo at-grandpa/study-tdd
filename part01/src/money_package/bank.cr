4122a2c52a30d5e9c94b41ca4ff53006c329fc76
yoshiyuki-tsuchida <yoshiyuki_tsuchida@voyagegroup.com>
2017-11-10 22:40:19 +0900

    addRateの空実装

--- part01/src/money_package/bank.cr
+++ part01/src/money_package/bank.cr
@@ -3,5 +3,8 @@ module MoneyPackage
     def reduce(source : Expression, to : String) : Money
       source.reduce(to)
     end
+
+    def addRate(from : String, to : String, rate : Int32) : Void
+    end
   end
 end
