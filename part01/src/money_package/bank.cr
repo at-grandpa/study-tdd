bf622244fb97820288790e9316e99c58c7ed253f
yoshiyuki-tsuchida <yoshiyuki_tsuchida@voyagegroup.com>
2017-11-10 09:25:34 +0900

    Expressionにreduceが定義されているので分岐処理は必要無くなった

--- part01/src/money_package/bank.cr
+++ part01/src/money_package/bank.cr
@@ -1,9 +1,7 @@
 module MoneyPackage
   class Bank
     def reduce(source : Expression, to : String) : Money
-      return source.as(Money).reduce(to) if source.is_a?(Money)
-      sum = source.as(Sum)
-      sum.reduce(to)
+      source.reduce(to)
     end
   end
 end
