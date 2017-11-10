8ec4fca9d69e3aabcb87fd7d69bacb6b439b85b1
yoshiyuki-tsuchida <yoshiyuki_tsuchida@voyagegroup.com>
2017-11-11 08:07:26 +0900

    Sumにplusの空実装

--- part01/src/money_package/sum.cr
+++ part01/src/money_package/sum.cr
@@ -10,6 +10,9 @@ module MoneyPackage
     def initialize(@augend : Expression, @addend : Expression)
     end
 
+    def plus(addned : Expression) : Expression
+    end
+
     def reduce(bank : Bank, to : String) : Money
       amount : Int32 = @augend.reduce(bank, to).@amount + @addend.reduce(bank, to).@amount
       Money.new(amount, to)
