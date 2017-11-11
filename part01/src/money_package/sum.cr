a85a07a7403f5389c324b5f5f3cad4bd7f64b7be
yoshiyuki-tsuchida <yoshiyuki_tsuchida@voyagegroup.com>
2017-11-11 11:32:22 +0900

    timesメソッドの実装

--- part01/src/money_package/sum.cr
+++ part01/src/money_package/sum.cr
@@ -10,6 +10,10 @@ module MoneyPackage
     def initialize(@augend : Expression, @addend : Expression)
     end
 
+    def times(multiplier : Int32) : Expression
+      Sum.new(@augend.times(multiplier), @addend.times(multiplier))
+    end
+
     def plus(addned : Expression) : Expression
       Sum.new(self, addend)
     end
