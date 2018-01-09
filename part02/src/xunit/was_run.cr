8e693cb2f6279710bff617de66a9c1d74f4ea14d
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2018-01-09 18:40:14 +0900

    TestCaseクラスを作成してリファクタリング

--- part02/src/xunit/was_run.cr
+++ part02/src/xunit/was_run.cr
@@ -1,20 +1,5 @@
-class WasRun
+class WasRun < TestCase
   getter was_run : Bool = false
-
-  def initialize(@name : String)
-  end
-
-  def run
-    {% begin %}
-      case @name
-        {% for method, index in @type.methods.map(&.name) %}
-          when {{method.stringify}} then {{method.id}}
-        {% end %}
-      else
-        raise "Undefined method. You specified: #{@name}"
-      end
-    {% end %}
-  end
 
   def test_method
     @was_run = true
