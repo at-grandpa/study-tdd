7c3e95901060d2947ccde4777855e078f14342a3
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2018-01-09 18:55:25 +0900

    TestCaseTestの追加 & assert文の代替

--- part02/src/xunit.cr
+++ part02/src/xunit.cr
@@ -1,9 +1,15 @@
 require "./xunit/*"
+require "spec/expectations"
+require "spec/methods"
+require "spec/dsl"
 
-module Xunit
+class TestCaseTest < TestCase
+  def test_running
+    test = WasRun.new("test_method")
+    test.was_run.should eq false
+    test.run
+    test.was_run.should eq true
+  end
 end
 
-test = WasRun.new("test_method")
-p test.was_run
-test.run
-p test.was_run
+TestCaseTest.new("test_running").run
