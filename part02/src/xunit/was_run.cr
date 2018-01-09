5107245b194e1a86fa82f33cfa3252c81c112a62
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2018-01-09 23:42:11 +0900

    was_setupの削除

--- part02/src/xunit/was_run.cr
+++ part02/src/xunit/was_run.cr
@@ -1,10 +1,8 @@
 class WasRun < TestCase
   getter was_run : Bool = false
-  getter was_setup : Bool = false
   getter log : String = ""
 
   def setup
-    @was_setup = true
     @log = "setUp "
   end
 
