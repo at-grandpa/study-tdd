575b76fe46b58df3c62b6d652d9b661afd07c752
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2018-01-10 10:15:18 +0900

    TestSuite#runを追加

--- part02/src/xunit/test_suite.cr
+++ part02/src/xunit/test_suite.cr
@@ -4,4 +4,12 @@ class TestSuite
   def add(test)
     @tests << test
   end
+
+  def run
+    result = TestResult.new
+    @tests.each do |test|
+      test.run(result)
+    end
+    result
+  end
 end
