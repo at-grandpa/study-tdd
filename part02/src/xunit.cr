c7a622c0468e9209661e1f20ebd08c69629e4dfe
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2018-01-10 12:11:34 +0900

    resultをsetupで生成した

--- part02/src/xunit.cr
+++ part02/src/xunit.cr
@@ -5,42 +5,42 @@ require "spec/dsl"
 
 class TestCaseTest < TestCase
   @test : WasRun = WasRun.new("")
+  @result : TestResult = TestResult.new
+
+  def setup
+    @result = TestResult.new
+  end
 
   def test_template_method
     @test = WasRun.new("test_method")
-    result = TestResult.new
-    @test.run(result)
+    @test.run(@result)
     @test.log.should eq "setUp testMethod tearDown "
   end
 
   def test_result
     @test = WasRun.new("test_method")
-    result = TestResult.new
-    @test.run(result)
-    result.summary.should eq "1 run, 0 failed"
+    @test.run(@result)
+    @result.summary.should eq "1 run, 0 failed"
   end
 
   def test_failed_result
     @test = WasRun.new("test_broken_method")
-    result = TestResult.new
-    @test.run(result)
-    result.summary.should eq "1 run, 1 failed"
+    @test.run(@result)
+    @result.summary.should eq "1 run, 1 failed"
   end
 
   def test_failed_result_formatting
-    result = TestResult.new
-    result.test_started
-    result.test_failed
-    result.summary.should eq "1 run, 1 failed"
+    @result.test_started
+    @result.test_failed
+    @result.summary.should eq "1 run, 1 failed"
   end
 
   def test_suite
     suite = TestSuite.new
     suite.add(WasRun.new("test_method"))
     suite.add(WasRun.new("test_broken_method"))
-    result = TestResult.new
-    suite.run(result)
-    result.summary.should eq "2 run, 1 failed"
+    suite.run(@result)
+    @result.summary.should eq "2 run, 1 failed"
   end
 end
 
